90c46a1327339c1aa37854a827652431fa18f6f2
huzc818 <464055812@qq.com>
2018-09-15 08:47:21 +0800

    修改webssh保证直接部署可用

--- static/js/main.js
+++ static/js/main.js
@@ -1,34 +1,44 @@
 function openTerminal(options) {
-    var client = new WSSHClient();
-    var term = new Terminal({cols: 80, rows: 24, screenKeys: true, useStyle: true});
-    term.on('data', function (data) {
-        client.sendClientData(data);
-    });
-    term.open();
-    $('.terminal').detach().appendTo('#term');
-    $("#term").show();
-    term.write('Connecting...');
-    client.connect({
-        onError: function (error) {
-            term.write('Error: ' + error + '\r\n');
-            console.debug('error happened');
-        },
-        onConnect: function () {
-            client.sendInitData(options);
-            client.sendClientData('\r');
-            console.debug('connection established');
-        },
-        onClose: function () {
-            term.write("\rconnection closed")
-            console.debug('connection reset by peer');
-            $('term').hide()
-        },
-        onData: function (data) {
-            term.write(data);
-            console.debug('get data:' + data);
-        }
-    })
+	var client = new WSSHClient();
+	var term = new Terminal({cols: 80, rows: 24, screenKeys: true, useStyle: true});
+	term.on('data', function (data) {
+		client.sendClientData(data);
+	});
+	term.open();
+	$('.terminal').detach().appendTo('#term');
+	$("#term").show();
+	term.write('Connecting...');
+	client.connect({
+		onError: function (error) {
+			term.write('Error: ' + error + '\r\n');
+			console.debug('error happened');
+			//$('#term').hide();
+			//$("#main").show();
+		},
+		onConnect: function () {
+			client.sendInitData(options);
+			client.sendClientData('\r');
+			console.debug('connection established');
+			$("#main").hide();
+			$('#term').show();
+		},
+		onClose: function () {
+			term.write("\rconnection closed")
+			console.debug('connection reset by peer');
+			//$('#term').hide();
+			//$("#main").show();
+		},
+		onData: function (data) {
+			term.write(data);
+			console.debug('get data:' + data);
+		}
+	})
 }
+
+setTimeout(function(){
+	$('#term').hide();
+	$("#main").show();
+},5000);
 
 var charWidth = 6.2;
 var charHeight = 15.2;
